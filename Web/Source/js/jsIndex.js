47c67eb4de02a3f3d1d35472c47bf8c0d6501e79
nartoan <toan.td.uet@gmail.com>
2016-10-24 13:31:55 +0700

    Upcode trang chủ

--- Web/Source/js/jsIndex.js
+++ Web/Source/js/jsIndex.js
@@ -1,5 +1,5 @@
 window.onclick = function(e) {
-    if (!e.target.matches('.notification') && !e.target.matches(".fa") && !e.target.matches(".name-noti")) {
+    if (!e.target.matches('.notification') && !e.target.matches(".fa-bell-o") && !e.target.matches(".name-noti")) {
         var dropdowns = document.getElementsByClassName("content-noti");
         for (var d = 0; d < dropdowns.length; d++) {
             var openDropdown = dropdowns[d];
@@ -18,6 +18,16 @@ window.onclick = function(e) {
             }
         }
     }
+
+    if (!e.target.matches('.search-box') && !e.target.matches(".fa-search") && !e.target.matches(".cheat-search")) {
+        var dropdowns = document.getElementsByClassName("list-event");
+        for (var d = 0; d < dropdowns.length; d++) {
+            var openDropdown = dropdowns[d];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
 }
 
 function showInformation() {
@@ -26,4 +36,8 @@ function showInformation() {
 
 function showNotification() {
     document.getElementById("dropdown-noti").classList.toggle("show");
+}
+
+function showAllEvent() {
+    document.getElementById("all-event").classList.toggle("show");
 }
