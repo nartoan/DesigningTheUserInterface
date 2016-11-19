573a69cbb2ff3f765ce8fb7c5b1bfdff3e79f749
nguyenducthuanuet <ndthuanuet@gmail.com>
2016-11-20 00:57:23 +0700

    update app android

---
+++ Android/Group8DTUI/app/src/main/java/com/androidbelieve/drawerwithswipetabs/MainActivity.java
@@ -0,0 +1,104 @@
+package com.androidbelieve.drawerwithswipetabs;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity{
+    DrawerLayout mDrawerLayout;
+    NavigationView mNavigationView;
+    FragmentManager mFragmentManager;
+    FragmentTransaction mFragmentTransaction;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /**
+         *Setup the DrawerLayout and NavigationView
+         */
+
+             mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+             mNavigationView = (NavigationView) findViewById(R.id.shitstuff) ;
+
+        /**
+         * Lets inflate the very first fragment
+         * Here , we are inflating the TabFragment as the first Fragment
+         */
+
+        /* spinner on nav_header*/
+        View mView = mNavigationView.getHeaderView(0);
+        AppCompatSpinner spinner = (AppCompatSpinner) mView.findViewById(R.id.username);
+// Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.soccer, R.layout.spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+        /**/
+
+             mFragmentManager = getSupportFragmentManager();
+             mFragmentTransaction = mFragmentManager.beginTransaction();
+             mFragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+        /**
+         * Setup click events on the Navigation View Items.
+         */
+
+             mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+             @Override
+             public boolean onNavigationItemSelected(MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();
+
+
+
+                 if (menuItem.getItemId() == R.id.nav_item_sent) {
+                     FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
+                     fragmentTransaction.replace(R.id.containerView,new SentFragment()).commit();
+
+                 }
+
+                if (menuItem.getItemId() == R.id.nav_item_inbox) {
+                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                }
+
+                 return false;
+            }
+
+        });
+
+        /**
+         * Setup Drawer Toggle of the Toolbar
+         */
+
+                android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+                ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
+                R.string.app_name);
+
+                mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+                mDrawerToggle.syncState();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
