e79703c926e692fe8e10dd9a25b0733a37246f24
thuanbv2203 <thuanbv2203@gmail.com>
2016-11-28 22:08:09 +0700

    edit something of layout

--- Android/Group8DTUI/app/src/main/java/com/androidbelieve/Group8/TinTuc/tab2Fragment.java
+++ Android/Group8DTUI/app/src/main/java/com/androidbelieve/Group8/TinTuc/tab2Fragment.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.androidbelieve.Group8.R;
 import com.daimajia.slider.library.Animations.DescriptionAnimation;
